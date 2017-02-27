58c8c721df2d59624fac21b3a5078e6190e2fd43
Brendan Sawyer (bxs8) <brendan.sawyer-contractor@nreca.coop>
2017-02-27 13:52:51 -0500

    Installed Firebase & AngularFire2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+
+// TODO: Update with actual Firebase config
+export const firebaseConfig = {
+  apiKey: '<your-key>',
+  authDomain: '<your-project-authdomain>',
+  databaseURL: '<your-database-URL>',
+  storageBucket: '<your-storage-bucket>',
+  messagingSenderId: '<your-messaging-sender-id>'
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +22,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
