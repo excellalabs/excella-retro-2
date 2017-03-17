82d938eca894add27ce344257cc45eaa518f0b53
bjsawyer <bjsawyer@vt.edu>
2017-03-16 21:07:23 -0400

    Added PhaseId to Message model

--- src/app/models/message.ts
+++ src/app/models/message.ts
@@ -2,12 +2,14 @@ export class Message {
   id: number;
   text: string;
   groupId: number;
+  phaseId: number;
   sessionId: string;
 
-  constructor(id: number, text: string, groupId: number, sessionId: string) {
+  constructor(id: number, text: string, groupId: number, phaseId: number, sessionId: string) {
     this.id = id;
     this.text = text;
     this.groupId = groupId;
+    this.phaseId = phaseId;
     this.sessionId = sessionId;
   }
 }
