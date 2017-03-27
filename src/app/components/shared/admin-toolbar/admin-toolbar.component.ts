64a8600e60651d512d194d202e8a9db8d45bc077
bjsawyer <Brendan.Sawyer-contractor@nreca.coop>
2017-03-26 21:38:10 -0400

    Added functionality for Copy Retro Id, End Phase, and End Retro

--- src/app/components/shared/admin-toolbar/admin-toolbar.component.ts
+++ src/app/components/shared/admin-toolbar/admin-toolbar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ViewEncapsulation, ChangeDetectionStrategy, Input } from '@angular/core';
-import { FirebaseObjectObservable } from 'angularfire2';
+import { AngularFire, FirebaseObjectObservable } from 'angularfire2';
+import { MdSnackBar } from '@angular/material';
 import { Retro } from '../../../models/retro';
+import { Phase } from '../../../models/phase';
 
 @Component({
   selector: 'app-admin-toolbar',
@@ -11,10 +13,36 @@ import { Retro } from '../../../models/retro';
 })
 export class AdminToolbarComponent implements OnInit {
   @Input() retroObservable: FirebaseObjectObservable<Retro>;
+  phaseObservable: FirebaseObjectObservable<Phase>;
 
-  constructor() { }
+  constructor(public snackbar: MdSnackBar, private af: AngularFire) { }
 
   ngOnInit() {
   }
 
+  copyTextSuccessful() {
+    this.snackbar.open('Text copied to clipboard!', 'OK', {
+      duration: 1000
+    })
+  }
+
+  endPhase() {
+    const self = this;
+
+    // TODO: Add modal to confirm End Phase
+
+    this.retroObservable.subscribe(retroVal => {
+      self.phaseObservable = this.af.database.object('phases/' + retroVal.currentPhaseId);
+      let phaseSubscription = self.phaseObservable.subscribe(phaseVal => {
+        let nextPhaseStep = phaseVal.currentPhaseStep += 1;
+        self.phaseObservable.update({ currentPhaseStep: nextPhaseStep });
+        phaseSubscription.unsubscribe();
+      });
+    });
+  }
+
+  endRetro() {
+    // TODO: Add modal to confirm End Retro
+    this.retroObservable.update({ isActive: false });
+  }
 }
