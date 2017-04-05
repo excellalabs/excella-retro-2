95119d9fb14066a15878115a5bf9262a821c07f3
samblissard <samblissard@gmail.com>
2017-04-05 17:16:51 -0600

    Starting to pull messages by retro ID

--- src/app/components/phase-steps/group-feedback/group-feedback.component.ts
+++ src/app/components/phase-steps/group-feedback/group-feedback.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+import { AngularFire, FirebaseObjectObservable, FirebaseListObservable } from 'angularfire2';
+import { Retro } from '../../../models/retro';
+import { Message } from '../../../models/message';
 
 @Component({
   selector: 'app-group-feedback',
@@ -8,10 +11,27 @@ import { Component, OnInit, ViewEncapsulation, ChangeDetectionStrategy } from '@
   changeDetection: ChangeDetectionStrategy.Default
 })
 export class GroupFeedbackComponent implements OnInit {
-
-  constructor() { }
+  @Input() data: FirebaseObjectObservable<Retro>;
+  retroObservable: FirebaseObjectObservable<Retro>;
+  retro: Retro;
+  retroId: string;
+  feedbackMessagesObservable: FirebaseListObservable<Message[]>;
+  feedbackMessages: Message[];
+  
+  constructor(private af: AngularFire) { }
 
   ngOnInit() {
+    const self = this;
+    this.data.subscribe(retroVal => {
+      self.retroId = retroVal.$key;
+      self.feedbackMessagesObservable = self.af.database.list('/messages');
+      self.feedbackMessagesObservable.subscribe(feedbackMessages => {
+        self.feedbackMessages = feedbackMessages.filter((feedback) => {
+          return feedback.retroId === self.retroId;
+        })
+      })
+    }
+    );
   }
 
 }
