6c603d56c5fed14f0210e3e8518f9747a1b62e3b
bjsawyer <bjsawyer@vt.edu>
2017-04-06 11:39:45 -0600

    Fixes for creation of retro

--- src/app/models/retro.ts
+++ src/app/models/retro.ts
@@ -1,13 +1,15 @@
 export class Retro {
   name: string;
   isActive: boolean;
+  currentPhaseId: string;
   currentPhaseNumber: number;
   votesPerParticipant: number;
   $key: string;
 
-  constructor(name: string, isActive: boolean, currentPhaseNumber: number, votesPerParticipant: number) {
+  constructor(name: string, isActive: boolean, currentPhaseId: string, currentPhaseNumber: number, votesPerParticipant: number) {
     this.name = name;
     this.isActive = isActive;
+    this.currentPhaseId = currentPhaseId;
     this.currentPhaseNumber = currentPhaseNumber;
     this.votesPerParticipant = votesPerParticipant;
   }
