fcf8df2012233a2869d740e813cbd4c368d5b30a
bjsawyer <bjsawyer@vt.edu>
2017-04-24 20:09:28 -0400

    Added retro summary routing

--- src/app/components/retro-complete/retro-complete.component.ts
+++ src/app/components/retro-complete/retro-complete.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-retro-complete',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./retro-complete.component.css']
 })
 export class RetroCompleteComponent implements OnInit {
+  retroId: string;
 
-  constructor() { }
+  constructor(private router: Router,
+    private route: ActivatedRoute,
+    private location: Location) { }
 
   ngOnInit() {
+    const self = this;
+    this.route.params.subscribe(params => self.retroId = params['retroId']);
   }
 
+  showRetroSummary() {
+    this.router.navigateByUrl('retro/${self.retroId}/summary');
+  }
 }
