f10d66134c30571b9b72eb555ca915b5d63f23b1
Alex Hoffman <alexhoffman617@gmail.com>
2017-04-26 20:45:00 -0400

    Initial summary page ui

--- src/app/components/retro-summary/retro-summary.component.ts
+++ src/app/components/retro-summary/retro-summary.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { RetroArchiveService } from '../../services/retro-archive.service';
+import { ArchivedRetro } from '../../models/archive/archived-retro';
+import { ArchivedGroup } from '../../models/archive/archived-group';
+import { ArchivedMessage } from '../../models/archive/archived-message';
+import { ArchivedPhase } from '../../models/archive/archived-phase';
+
 
 @Component({
   selector: 'app-retro-summary',
@@ -9,12 +14,25 @@ import { RetroArchiveService } from '../../services/retro-archive.service';
 })
 export class RetroSummaryComponent implements OnInit {
   retroId: string;
-
+  archivedRetro: ArchivedRetro;
   constructor(private route: ActivatedRoute,
     private retroArchiveService: RetroArchiveService) { }
 
   ngOnInit() {
     const self = this;
+    var ar = new ArchivedRetro;
+    ar.retroId = "RETRO ID";
+    var archivedMessage = new ArchivedMessage;
+    archivedMessage.text = "TEXT";
+    var archivedGroup = new ArchivedGroup;
+    archivedGroup.name = "GROUP NAME";
+    archivedGroup.archivedMessages = [archivedMessage, archivedMessage];
+    archivedGroup.numOfVotes = 3;
+    var archivedPhase = new ArchivedPhase;
+    archivedPhase.name = "PHASE NAME";
+    archivedPhase.archivedGroups = [archivedGroup, archivedGroup];
+    ar.archivedPhases = [archivedPhase, archivedPhase];
+    self.archivedRetro = ar;
     this.route.params.subscribe(params => self.retroId = params['retroId']);
     this.retroArchiveService.getArchivedRetroById(this.retroId);
   }
