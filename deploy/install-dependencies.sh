afc623b21aebdb4c64197a6c30d1f078e5021846
bjsawyer <bjsawyer@vt.edu>
2017-05-17 20:20:41 -0400

    Install docker-compose

--- deploy/install-dependencies.sh
+++ deploy/install-dependencies.sh
@@ -1,24 +1,3 @@
 #!/bin/bash
 
-# Update packages
-apt-get update
-
-# Install curl
-apt-get install \
-  apt-transport-https \
-  ca-certificates \
-  curl \
-  software-properties-common
-
-# Install python
-apt-get install python3
-
-# Install pip
-curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
-python get-pip.py
-
-# Install AWS CLI
-pip install awscli
-
-# Install Docker
-apt-get install docker-ce
+pip install docker-compose
