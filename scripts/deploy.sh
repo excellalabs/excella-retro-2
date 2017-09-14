ed88c6395a76f71503a16ddff7c2de731fead356
bjsawyer <bjsawyer@vt.edu>
2017-09-13 21:34:44 -0400

    Fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
 aws ecr get-login --no-include-email --region $AWS_REGION
-docker image push $AWS_IMAGE_URL
+docker image push $AWS_REPO_URL
 docker run silintl/ecs-deploy -c $AWS_ECS_CLUSTER -n $AWS_SERVICE_NAME -i $AWS_REPO_URL:latest -r $AWS_REGION --timeout 1000 -k $AWS_ACCESS_KEY_ID -s $AWS_SECRET_ACCESS_KEY
