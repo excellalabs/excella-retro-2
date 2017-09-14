a354f6f15e84bf7b5005b6dcc3eb930aa2aa6241
bjsawyer <bjsawyer@vt.edu>
2017-09-13 22:24:00 -0400

    Removing unneeded keys (implicitly pulls those env vars)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
 aws ecr get-login --no-include-email --region $AWS_REGION
 docker image push $AWS_REPO_URL
-sudo docker run silintl/ecs-deploy -k $AWS_ACCESS_KEY_ID -s $AWS_SECRET_ACCESS_KEY -c $AWS_ECS_CLUSTER -n $AWS_SERVICE_NAME -i $AWS_REPO_URL:latest -r $AWS_REGION --verbose --timeout 1000
+docker run silintl/ecs-deploy -c $AWS_ECS_CLUSTER -n $AWS_SERVICE_NAME -i $AWS_REPO_URL:latest --timeout 1000
