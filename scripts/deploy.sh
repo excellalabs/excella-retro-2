0832eab8833f867e4aaf3d37a72d3a7367e14c1c
bjsawyer <bjsawyer@vt.edu>
2017-09-14 21:20:49 -0400

    Setting travis to node 8.4

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,3 @@
 aws ecr get-login --no-include-email --region $AWS_REGION
 docker image push $AWS_REPO_URL
-echo $AWS_ACCESS_KEY_ID
-echo $AWS_DEFAULT_REGION
 docker run silintl/ecs-deploy -k $AWS_ACCESS_KEY_ID -s $AWS_SECRET_ACCESS_KEY -r $AWS_DEFAULT_REGION -c $AWS_ECS_CLUSTER -n $AWS_SERVICE_NAME -i $AWS_REPO_URL:latest --timeout 1000
