76485232b1c1d59c01ecee8697d88e9b254957bc
bjsawyer <Brendan.Sawyer-contractor@nreca.coop>
2017-10-18 17:09:27 -0400

    Fix #2

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,7 @@
+set -ev
+AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+
 aws ecr get-login --no-include-email --region $AWS_DEFAULT_REGION
 docker image push $AWS_REPO_URL
-docker run -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY silintl/ecs-deploy -r $AWS_DEFAULT_REGION -c $AWS_ECS_CLUSTER -n $AWS_SERVICE_NAME -i $AWS_REPO_URL:latest --timeout 1000
+docker run silintl/ecs-deploy -r $AWS_DEFAULT_REGION -c $AWS_ECS_CLUSTER -n $AWS_SERVICE_NAME -i $AWS_REPO_URL:latest --timeout 1000
